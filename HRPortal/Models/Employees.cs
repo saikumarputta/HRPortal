a3b5a082ba32d5a6b2240e92c695439d51083e30
saikumarputta <skputta@R8YN0YK-PC>
2019-10-24 17:45:39 +0530

    del emp

--- HRPortal/Models/Employees.cs
+++ HRPortal/Models/Employees.cs
@@ -7,8 +7,6 @@ namespace HRPortal.Models
 {
     public class Employees
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Address  { get; set; }
+       
     }
 }
