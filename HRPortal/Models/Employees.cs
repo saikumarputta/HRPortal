e3b6836add7f72e9b9254492735a0bfde38861cb
Haritha Gonuguntla <hgonuguntla@deltaintech.com>
2019-10-25 16:50:43 +0530

    employee model

---
+++ HRPortal/Models/Employees.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRPortal.Models
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+    }
+}
