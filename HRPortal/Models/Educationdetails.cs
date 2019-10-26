135e6a9966731e5577834b26e0d98bda57a83abd
Haritha Gonuguntla <hgonuguntla@deltaintech.com>
2019-10-26 12:41:25 +0530

    hgh

---
+++ HRPortal/Models/Educationdetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRPortal.Models
+{
+    public partial class Educationdetails
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int EmployeeId { get; set; }
+        [Required]
+        public string Education { get; set; }
+        [Required]
+        public string InstituteUniversity { get; set; }
+        public int YearOfPass { get; set; }
+        public string Comments { get; set; }
+
+        public Employee Employee { get; set; }
+    }
+}
---
+++ HRPortal/Models/Experiencedetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRPortal.Models
+{
+    public partial class Experiencedetails
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int EmployeeId { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+        [Required]
+        public DateTime StartYear { get; set; }
+        public DateTime? EndYear { get; set; }
+
+        public Employee Employee { get; set; }
+    }
+}
